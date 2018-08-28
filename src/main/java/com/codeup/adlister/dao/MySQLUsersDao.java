a2e93266db2d1565c07d4919f0b2597786a74111
JRihn32 <justin.rihn32@gmail.com>
2018-08-28 17:05:32 -0500

    edited login, profile, register to match wireframe

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,20 +1,23 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
-
+import com.codeup.adlister.dao.MySQLAdsDao;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLUsersDao implements Users {
-    private Connection connection;
+    private static Connection connection;
 
     public MySQLUsersDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
-                config.getUrl(),
-                config.getUser(),
-                config.getPassword()
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
             );
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to the database!", e);
@@ -51,16 +54,63 @@ public class MySQLUsersDao implements Users {
         }
     }
 
-    private User extractUser(ResultSet rs) throws SQLException {
+    @Override
+    public User findByUserId(long userId) {
+        String query = "SELECT * FROM users WHERE id = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setLong(1, userId);
+            ResultSet rs = stmt.executeQuery();
+            return extractUser(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("error finding user id",e);
+        }
+
+    }
+
+    @Override
+    public List<User> searchUser(String searchTerm) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM users WHERE username LIKE ?");
+            stmt.setString(1, "%" + searchTerm + "%");
+            ResultSet rs = stmt.executeQuery();
+            return searchUsersByName(rs);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("error searching user");
+        }
+    }
+
+    private static User extractUser(ResultSet rs) throws SQLException {
         if (! rs.next()) {
             return null;
         }
         return new User(
-            rs.getLong("id"),
-            rs.getString("username"),
-            rs.getString("email"),
-            rs.getString("password")
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
         );
+    }
+
+//    public static List<Ad> findAdByUsername(User username) {
+//        PreparedStatement stmt;
+//        try {
+//            stmt = connection.prepareStatement("SELECT * FROM ads WHERE user_id LIKE ?");
+//            stmt.setLong(1, username.getId());
+//            ResultSet rs = stmt.executeQuery();
+//            return MySQLAdsDao.createAdsFromResults(rs);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error retrieving your ads.", e);
+//        }
+//    }
+    public static List<User> searchUsersByName(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while(rs.next()){
+            users.add(extractUser(rs));
+        }
+        return users;
     }
 
 }
